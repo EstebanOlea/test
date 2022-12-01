bdf3e312b27b375d3fe88cb16d243e0c71c5ce5d
EstebanOlea <114716953+EstebanOlea@users.noreply.github.com>
2022-12-01 06:46:11 -0600

    Initial commit

---
+++ questions.js
@@ -0,0 +1,480 @@
+let MCQS = [{
+    question: "¿de las siguientes expresiones es equivalente a 3x(2+5y)?",
+    choice1: "3x+5",
+    choice2: "6x+15xy",
+    choice3: "6x+5y",
+    choice4: "5x+15y",
+    answer: 1
+},
+            {
+            question: "¿cual es la solucion de la siguiente ecuacion 3x=-2?",
+    choice1: "x=-2/3",
+    choice2: "x=-3/2",
+    choice3: "x=-2",
+    choice4: "y=2",
+    answer: 2
+},
+            {
+            question: "¿Cual es la solucion de la siguiente ecuacion 5x-25=0?",
+    choice1: "x=5",
+    choice2: "x=-5",
+    choice3: "x=1/5",
+    choice4: "x=y",
+    answer: 1
+},
+{
+question: "¿Cual es la solucion de la siguiente ecuacion x+1=2-x?",
+choice1: "x=1/2",
+choice2: "x=-2",
+choice3: "x=2",
+choice4: "x=5",
+answer: 1
+},
+{
+question: "¿Cual es la solucion de la siguiente ecuacion 2x-2=-2(1-x)?",
+choice1: "x=0",
+choice2: "x=-2",
+choice3: "x=no tiene solucion",
+choice4: "x=x-2",
+answer: 2
+},
+{
+question: "¿Cual es la solucion de la siguiente ecuacion 3x-1=x+1+2x?",
+choice1: "x=0",
+choice2: "x=no tiene solucion",
+choice3: "x=2/3",
+choice4: "x=-2",
+answer: 1
+},
+{
+question: "¿Cual es la solucion de la siguiente ecuacion (x-1/2)=2x",
+choice1: "x=1/3",
+choice2: "x=-1/3",
+choice3: "x=-2/3",
+choice4: "x=2",
+answer: 1
+},
+{
+question: "¿Cual es la solucion de la siguiente ecuacion 2*(x-1/2)=x+1?",
+choice1: "x=2",
+choice2: "x=no tiene aolucion",
+choice3: "x=-2",
+choice4: "x=1/2",
+answer: 1
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion x/3=x+1/6?",
+    choice1: "x=-1",
+    choice2: "x=1",
+    choice3: "x=3",
+    choice4: "x=no tiene solucion",
+    answer: 1
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion 3*(x-2)-3=x?",
+    choice1: "9/2",
+    choice2: "Todo numero real es la solucion",
+    choice3: "-9/2",
+    choice4: "no tiene solucion",
+    answer: 0
+},
+{
+    question: "¿Cual de las siguientes ecuaciones significa la suma de dos numeros consecutivos es igual a 19?",
+    choice1: "(x+x)+2=19",
+    choice2: "x+(x+1)=19",
+    choice3: "-ninguna de las anteriores",
+    choice4: "ambas opciones",
+    answer: 1
+},
+{
+    question: "¿Cual de las siguientes ecuaciones significa la cuarta parte del consecutivo de un numero es igual a 5?",
+    choice1: "x/4+1=5",
+    choice2: "x+1/4=5",
+    choice3: "x/4-1=5",
+    choice4: "x=no tiene solucion",
+    answer: 1
+},
+{
+    question: "¿Cual de las siguientes ecuaciones significa tres cuartas partes del triple de un numero es igual a 6?",
+    choice1: "3*x/4=6",
+    choice2: "3/4*3x=6",
+    choice3: "3x+x/4=6",
+    choice4: "todas las ateriores",
+    answer: 1
+},
+{
+    question: "¿Que edad tiene joel si dentro de 5 años su edad sera el doble de la actual?",
+    choice1: "2x+5=x",
+    choice2: "x+5=2x+5",
+    choice3: "2x=x+5",
+    choice4: "5+2x=x",
+    answer: 2
+},
+{
+    question: "¿seleccione que ecuacion es correcta?",
+    choice1: "2*x+3/9=3",
+    choice2: "2)*(x+3)=3",
+    choice3: "2x+3/9=3",
+    choice4: "3=9+2x/3",
+    answer: 2
+},
+{
+    question: "¿Si y=3ab+2b^3, a que equivale y cuando a=1 y b=2?",
+    choice1: "16",
+    choice2: "18",
+    choice3: "20",
+    choice4: "22",
+    answer: 3
+},
+{
+    question: "¿Cuantas contraseñas se pueden formar del conjunto de letras {A,B,C,D} si cada letra se puede usar solo una vez?",
+    choice1: "6",
+    choice2: "24",
+    choice3: "120",
+    choice4: "720",
+    answer: 1
+},
+{
+    question: "¿Factoriza 4y(3x+2)-2(3x+2)?",
+    choice1: "(4y-2)(3x+2)",
+    choice2: "(3x-2)(4y+2)",
+    choice3: "(4y+2)(6x)",
+    choice4: "(3x+2)(4y+2)",
+    answer: 0
+},
+{
+    question: "¿Si a=-4,y b=3 cual es |a-b|?",
+    choice1: "1",
+    choice2: "2",
+    choice3: "4",
+    choice4: "7",
+    answer: 3
+},
+{
+    question: "¿Si 10 pulgadas en un mapa representan 100 pies,que distancia real es representada por 25 pulgadas ?",
+    choice1: "25 pies",
+    choice2: "100 pies",
+    choice3: "150 pies",
+    choice4: "250 pies",
+    answer: 3
+},
+{
+    question: "¿Hace 5 años Amy tenia 3 veces mas que mike,si mike tiene 10 años ahora, que edad tiene Amy?",
+    choice1: "16",
+    choice2: "20",
+    choice3: "25",
+    choice4: "30",
+    answer: 1
+},
+{
+    question: "¿Cual es la probabilidad de seleccionar un hombre de un grupo de 4 hombres y 8 mujeres?",
+    choice1: "1/3",
+    choice2: "1/2",
+    choice3: "2/3",
+    choice4: "4/5",
+    answer: 0
+},
+{
+    question: "¿Cuales son las soluciones a la siguiente ecuacion x^2+8x+15=0?",
+    choice1: "(2,3)",
+    choice2: "(3,5)",
+    choice3: "(-2,-3)",
+    choice4: "(-3,-5)",
+    answer: 3
+},
+{
+    question: "¿Cual es la pendiente de la linea y=3x-7?",
+    choice1: "2",
+    choice2: "3",
+    choice3: "5",
+    choice4: "7",
+    answer: 1
+},
+{
+    question: "¿Cuantos diferentes comites diferentes de personas se pueden formar seleccionando de un grupo de 5 personas?",
+    choice1: "3",
+    choice2: "5",
+    choice3: "10",
+    choice4: "20",
+    answer: 2
+},
+{
+    question: "¿(3x+4y)(2x+5y=)?",
+    choice1: "(3x^2+8xy+20y^2)",
+    choice2: "(3x^2+15xy+30y^2)",
+    choice3: "(6x^2+15xy+20y^2)",
+    choice4: "(6x^2+23xy+20y^2)",
+    answer: 3
+},
+{
+    question: "¿(Cual es la ecuacion de la linea que pasa a travez de los puntos A(1,2) y B(4,3)?",
+    choice1: "y=x+5",
+    choice2: "y=1/3x+5",
+    choice3: "y=1/3x+5/3",
+    choice4: "y=x+5/3",
+    answer: 2
+},
+{
+    question: "¿Dado f(x)=x^2+4x-3 Encuentre f(-3)?",
+    choice1: "-3",
+    choice2: "-6",
+    choice3: "0",
+    choice4: "18",
+    answer: 1
+},
+{
+    question: "¿44 es 80% de que numero?",
+    choice1: "51",
+    choice2: "53",
+    choice3: "55",
+    choice4: "57",
+    answer: 2
+},
+{
+    question: "¿Resuelva para x, 5x-5(x+2)<=4+4(4-1/8x)?",
+    choice1: "x>=3",
+    choice2: "x<=3",
+    choice3: "x<=4.5",
+    choice4: "x>=4.5",
+    answer: 1
+},
+{
+    question: "¿Derive la siguiente ecuacion f(x)=5?",
+    choice1: "x",
+    choice2: "x=5",
+    choice3: "5",
+    choice4: "0",
+    answer: 3
+    },
+{
+    question: "¿Derive la siguiente ecuacion f(x)-2x?",
+    choice1: "-2",
+    choice2: "0",
+    choice3: "5",
+    choice4: "x",
+    answer: 0
+},
+{
+    question: "¿Derive la siguiente ecuacion f(x)-2x+2?",
+    choice1: "0",
+    choice2: "x",
+    choice3: "y",
+    choice4: "-2",
+    answer: 3
+},
+{
+    question: "¿Derive la siguiente ecuacion f(x)-2x^2-5?",
+    choice1: "5",
+    choice2: "-2",
+    choice3: "-4x",
+    choice4: "x",
+    answer: 2
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 2-x=x-8?",
+    choice1: "x=5",
+    choice2: "x=3",
+    choice3: "x=2",
+    choice4: "no tiene solucion",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 2x-1=5x+8?",
+    choice1: "x=-3",
+    choice2: "x=2",
+    choice3: "x=-5",
+    choice4: "no tiene solicion",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 3+3x-1=x+2+2x?",
+    choice1: "0=0",
+    choice2: "x=3",
+    choice3: "x=2",
+    choice4: "x no tiene solucion",
+    answer: 0
+},
+{
+    question: "¿Derive la siguiente ecuacion f(x)-2x^4+x^3-x^2+4?",
+    choice1: "8x^3+3x^2-2x",
+    choice2: "2x^3-6x^2+2",
+    choice3: "x=2",
+    choice4: "3x^2+2x^3-2",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 2(1+2x)=10?",
+    choice1: "x=2",
+    choice2: "x=8/4=2",
+    choice3: "4x=8",
+    choice4: "x=4x",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 2(3x-2)=2?",
+    choice1: "x=1",
+    choice2: "x=6/6",
+    choice3: "x=6x-4=2",
+    choice4: "x=0",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion -2(3x-2)=-2?",
+    choice1: "x=-6/-6=1",
+    choice2: "-6x=x",
+    choice3: "x=2",
+    choice4: "no tiene solucion",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 1-x/3=5x/3?",
+    choice1: "x=2/6=1/2",
+    choice2: "3x=5",
+    choice3: "-3x=5",
+    choice4: "y=0",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 2x/3+16/3=-4x/2?",
+    choice1: "x=-32/16=-2",
+    choice2: "x=32/-15=-y",
+    choice3: "y=32",
+    choice4: "16x=-32",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 1+1/2(4x-6=-2)?",
+    choice1: "x=0/2=0",
+    choice2: "2x=0",
+    choice3: "x=2",
+    choice4: "z=20",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 5-2(1-x)=2x-3?",
+    choice1: "no tiene solucion",
+    choice2: "x=-6",
+    choice3: "3x+2",
+    choice4: "xx+3y",
+    answer: 0
+},
+{
+    question: "¿Resuelve la siguiente ecuacion 1/2(4x/3-4=0)?",
+    choice1: "4x/3=12",
+    choice2: "x=9",
+    choice3: "x=2",
+    choice4: "4x/3y",
+    answer: 1
+},
+{
+    question: "¿Derive la siguiente ecuacion f(x)-2x^4+x^3-x^2+4?",
+    choice1: "8x^3+3x^2-2x",
+    choice2: "2x^3-6x^2+2",
+    choice3: "x=2",
+    choice4: "3x^2+2x^3-2",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 3x^2+2=0?",
+    choice1: "x=-2/3",
+    choice2: "x=0",
+    choice3: "x=-2",
+    choice4: "no tiene solucion",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 6x^2-2x=0?",
+    choice1: "x=1/3",
+    choice2: "x=0",
+    choice3: "3x=1",
+    choice4: "4x=2",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 2x^2-5x=0?",
+    choice1: "x=5/2",
+    choice2: "2x=5",
+    choice3: "x^2",
+    choice4: "3x^2",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 3x-8x^2=0?",
+    choice1: "x=3/8",
+    choice2: "x=0",
+    choice3: "8x=3",
+    choice4: "no tiene solucion",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica x-x^2=0",
+    choice1: "x=-1",
+    choice2: "x=2",
+    choice3: "x=-2",
+    choice4: "x=x^3",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 9x^2-3=0?",
+    choice1: "x=1/3",
+    choice2: "x=0",
+    choice3: "x=3x=1",
+    choice4: "y=x^2",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 7x-21x^2=0?",
+    choice1: "x=1/3",
+    choice2: "x=4",
+    choice3: "x=-0",
+    choice4: "no tiene solucion",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 15x^2=5x?",
+    choice1: "x=1/3",
+    choice2: "x=0",
+    choice3: "x=-0",
+    choice4: "5x.(x3)",
+    answer: 0
+   }   ,
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 8x=-x?",
+    choice1: "x=-8",
+    choice2: "8+x",
+    choice3: "x=xy",
+    choice4: "no tiene solucion",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica x^2-4=0?",
+    choice1: "x=+-2",
+    choice2: "x1=2",
+    choice3: "x2=-2",
+    choice4: "x2=4",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 2x^2-18=0?",
+    choice1: "x=+-3",
+    choice2: "x=9",
+    choice3: "x18/2",
+    choice4: "x=-3",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 3x^2+48=0?",
+    choice1: "x^2=-48/3",
+    choice2: "x^2=48/3",
+    choice3: "x=48",
+    choice4: "x=4",
+    answer: 0
+},
+{
+    question: "¿Cual es la solucion de la siguiente ecuacion cuadratica 9-x^2=0?",
+    choice1: "x=+-3",
+    choice2: "x=9",
+    choice3: "x=-3",
+    choice4: "x=4",
+    answer: 0
+}];
---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'C:\Users\esteb\OneDrive\Escritorio\test',
+	globPatterns: [
+		'**/*.{jpg,html,json,js,css}'
+	],
+	swDest: 'C:/Users/esteb/OneDrive/Escritorio/test/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
